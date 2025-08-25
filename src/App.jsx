d88985b8901a5dd35ead2ce808e19d58ee9892a6
Umerkhx <uks38182@gmail.com>
2025-08-25 17:28:21 +0500

    zentry-banner

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Hero from './component/Hero'
+
+function App() {
+
+  return (
+    <>
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Hero/>
+    </main>
+
+    </>
+  )
+}
+
+export default App
